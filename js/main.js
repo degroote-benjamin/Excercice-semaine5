e104564c77b6d152272b6a367f2fcfb0eaaa13b0
benjamin <degroote.benjamin@gmail.com>
2017-07-25 14:06:40 +0200

    exo 1 part 2 finish

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var homme = {
     prenom: "ben",
     nom: "degroote",
-    pays: "france",
+    pays: "FRANCE",
     age: 24,
-    payement: ["visa", "mastercard"],
+    payement: ["VISA", "MASTERCARD"],
     getprenom: function() {
         alert(this.prenom);
     },
@@ -18,3 +18,41 @@ var homme = {
 homme.getprenom()
 homme.getnom()
 homme.getnai()
+
+
+var autoriser = {
+  paysautoriser : ["FRANCE","BELGIQUE","SUISSE"],
+  payementautoriser : ["VISA","CB","MASTERCARD"],
+  agemin : 10,
+  getage:function(){
+    if(this.agemin < homme.age){
+      alert("vous avez le bon age")
+    }
+    else{
+      alert("vous êtes trop jeune")
+    }
+  },
+  getpays:function(){
+    for(let i=0;i<this.paysautoriser.length;i++){
+      if(homme.pays.toUpperCase() === this.paysautoriser[i]){
+        alert("votre pays est autoriser")
+      }
+    }
+  },
+  getpay:function(){
+    var compt =0
+    for(let i=0;i<homme.payement.length;i++){
+      for(let y=0;y<this.payementautoriser.length;y++){
+        if(this.payementautoriser[y] === homme.payement[i].toUpperCase()){
+          compt++
+        }
+      }
+    }
+    if(compt >= 1){
+      alert("vos moyen de payement sont autoriser")
+    }
+  }
+}
+autoriser.getage()
+autoriser.getpays()
+autoriser.getpay()
